c7ea23c85cf3978075aabb3f81b9135bd66f1978
ThaHobbyist <dipdeys.dey@gmail.com>
2022-05-21 17:05:27 +0530

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="root">
+    <div class="heading">
+      <h3 class="heading_text">SASHAKTI</h3>
+      <h5 class="subheading_tex">
+        An indegenous Freelancing and learning platform
+      </h5>
+    </div>
+    <div class="reg">
+      <button class="login">
+        <h5>Sign-In/Register</h5>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="root">
+    <AuthBox />
+  </div>
+</template>
+
+<script>
+import AuthBox from "../components/AuthBox.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Loginview",
+  components: { AuthBox },
+};
+</script>
